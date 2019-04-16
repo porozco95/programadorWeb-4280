c2366850d820d7ab5453c1f56f3279d740c0c6cf
Patricio Orozco <porozco.x@gmail.com>
2019-04-16 20:25:39 -0300

    Mini Desafio Terminado

---
+++ Clase 03/Alumno/Mini ejercicio/js/index.js
@@ -0,0 +1,7 @@
+console.log('Estoy aqui...')
+var password = prompt ('Ingrese su contraseña:')
+
+while (password == null || password == undefined || password === ''){
+    password = prompt ('Ingrese su contraseña por favor:')
+}
+console.log('Gracias por ingresar su contraseña :)')
