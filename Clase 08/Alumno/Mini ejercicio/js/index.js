14ee9e1db731b628759eae4183e96b1e27a739fb
Patricio Orozco <porozco.x@gmail.com>
2019-05-07 19:33:27 -0300

    Mini Desafio Realizado

---
+++ Clase 08/Alumno/Mini ejercicio/js/index.js
@@ -0,0 +1,5 @@
+var newTitle = document.getElementById('title')
+var newText = document.getElementById('text')
+
+newTitle.innerHTML = prompt('Ingrese su nuevo Titulo: ')
+newText.innerHTML = prompt('Ingrese su nuevo Texto: ')
